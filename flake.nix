94926e35f73d03e1838d7eb307aaf86b809f66b1
Victor Mignot <root@victormignot.fr>
2022-09-21 23:10:56 -0400

    Init flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Discord Bot managing users interests.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {self, nixpkgs, rust-overlay, flake-utils, ...}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            (
+              rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ]; 
+              }
+            )
+          ];
+
+          packages = with pkgs; [
+            rust-analyzer
+            rustfmt
+          ];
+        };  
+      }
+    );
+}

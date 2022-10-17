42b7d04e32dc2b0125abdb95fd09e5e8f65df6bb
Victor Mignot <root@victormignot.fr>
2022-10-17 00:13:26 -0400

    Add Discord connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use yorokobot::client::{Client, ClientOptions};
+
+#[tokio::main]
+async fn main() {
+    let discord_token = env::var("DISCORD_TOKEN").expect("Cannot fetch Discord token");
+
+    let options = ClientOptions {
+        discord_token: &discord_token,
+    };
+
+    let mut client = Client::new(options).await;
+
+    client.connect_discord().await;
 }

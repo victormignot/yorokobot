e951b3012e043b55a1f279d4616085c5a653bcd4
Victor Mignot <root@victormignot.fr>
2022-10-18 23:23:54 -0400

    Provide basic MongoDB connection

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use std::env;
 
-use yorokobot::client::{Client, ClientCredentials};
+use yorokobot::{
+    client::{Client, ClientCredentials},
+    errors::ClientsError,
+};
 
 #[tokio::main]
 async fn main() {
     let discord_token = env::var("DISCORD_TOKEN").expect("Cannot fetch Discord token");
 
+    let mongodb_uri = env::var("MONGODB_URI").expect("Cannot fetch Mongo URI");
+
     let credentials = ClientCredentials {
         discord_token: &discord_token,
+        mongo_uri: &mongodb_uri,
     };
 
-    let mut client = Client::new(credentials).await;
+    let mut client = Client::new(credentials)
+        .await
+        .expect("Could not create client");
 
-    client.connect_discord().await;
+    client.connect().await.unwrap_or_else(|error| match error {
+        ClientsError::Database(e) => panic!("Could not connect to database: {:?}", e),
+        ClientsError::Discord(e) => panic!("Could not connect to Discord: {:?}", e),
+    });
 }
